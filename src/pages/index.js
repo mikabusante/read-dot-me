c0487611e0861b2c0a51fadbf443a2d9a5e40e6a
Mika Busante <mika@mikabusante.com>
2019-05-10 14:09:04 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,83 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+import styled from "styled-components"
+import { Waypoint } from "react-waypoint"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Section from "../components/Section"
+import Sidebar from "../components/Sidebar"
+import "../utils/index.css"
+import milkman from "../images/milkman.jpg"
+import mars from "../images/mars.jpg"
+import rest from "../images/rest.jpg"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const Site = styled.div`
+  font-family: sans-serif;
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  background: ${props => props.color};
+  transition-property: background;
+  transition-duration: 0.5s;
+`
 
-export default IndexPage
+const index = () => {
+  const [color, setColor] = useState(null)
+  const [position, setPosition] = useState(0)
+
+  const toggleAnimation = (color, position) => {
+    setColor(color)
+    setPosition(position)
+  }
+
+  return (
+    <Site color={color}>
+      <Sidebar color={color} position={position} />
+      <main>
+        <section>
+          <Waypoint
+            onEnter={() => toggleAnimation("pink", 1)}
+            bottomOffset="50%"
+          />
+          <Section
+            image={milkman}
+            place="1"
+            title="Milkman"
+            by="Anna Burns"
+            about="The last thing she ever wanted to be. To be interesting is to be
+          noticed and to be noticed is dangerous. Milkman is a tale of gossip
+          and hearsay, silence and deliberate deafness. It is the story of
+          inaction with enormous consequences."
+          />
+        </section>
+
+        <section>
+          <Waypoint
+            onEnter={() => toggleAnimation("orange", 2)}
+            bottomOffset="50%"
+          />
+          <Section
+            image={mars}
+            place="2"
+            title="The Mars Room"
+            by="Rachel Kushner"
+            about="It’s 2003 and Romy Hall is at the start of two consecutive life sentences at Stanville Women’s Correctional Facility, deep in California’s Central Valley. Outside is the world from which she has been severed: the San Francisco of her youth and her young son, Jackson. Inside is a new reality: thousands of women hustling for the bare essentials needed to survive; the bluffing and pageantry and casual acts of violence by guards and prisoners alike; and the deadpan absurdities of institutional living, which Kushner evokes with great humor and precision."
+          />
+        </section>
+
+        <section>
+          <Waypoint
+            onEnter={() => toggleAnimation("#C4406D", 3)}
+            bottomOffset="50%"
+          />
+          <Section
+            image={rest}
+            place="3"
+            title="The Mars Room"
+            by="Rachel Kushner"
+            about="It’s 2003 and Romy Hall is at the start of two consecutive life sentences at Stanville Women’s Correctional Facility, deep in California’s Central Valley. Outside is the world from which she has been severed: the San Francisco of her youth and her young son, Jackson. Inside is a new reality: thousands of women hustling for the bare essentials needed to survive; the bluffing and pageantry and casual acts of violence by guards and prisoners alike; and the deadpan absurdities of institutional living, which Kushner evokes with great humor and precision."
+          />
+        </section>
+      </main>
+    </Site>
+  )
+}
+
+export default index
