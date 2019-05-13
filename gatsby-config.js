072676211e4cc411a12448f3128bc52c2b625bb2
Mika Busante <mika@mikabusante.com>
2019-05-13 11:48:23 -0700

    Fixing some bug and organzing files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `README`,
+    description: `A layout experiment for a website showcasing the year's best books.`,
+    author: `@mikabusante`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-lodash`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
